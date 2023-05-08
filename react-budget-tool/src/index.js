b539c1a500ce87ea44f8a9bc25376b6e43defdf4
Randy Chu <randychu7@gmail.com>
2023-05-08 16:34:40 -0500

    done

--- react-budget-tool/src/index.js
+++ react-budget-tool/src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import App from './App';
+import App from './App.js';
 
+//createRoot means it tells react to make this the main place to render stuff through JS.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
