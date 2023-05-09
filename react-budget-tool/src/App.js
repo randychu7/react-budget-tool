d3ac9b7bef4d1a5740c9c3b5a8161eadf6cacc8b
Randy Chu <randychu7@gmail.com>
2023-05-09 15:25:54 -0500

    done

--- react-budget-tool/src/App.js
+++ react-budget-tool/src/App.js
@@ -1,9 +1,45 @@
+import Expenses from "../src/components/Expenses/Expenses";
+import Card from '../src/components/UI/Card';
+import React from 'react';
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+  const expenses = [
+    {
+    title: 'Car Insurance', 
+    amount:'267.34', 
+    date:new Date(2023,5,8)
+  },
+  {
+    title: 'Car Payment', 
+    amount:'600.34', 
+    date:new Date(2023,5,8)
+  },
+  {
+    title: 'Rent', 
+    amount:'1600.34', 
+    date:new Date(2023,5,1)
+  },
+  {
+    title: 'Hulu', 
+    amount:'5.99', 
+    date:new Date(2023,5,1)
+  }
+  ]
+return React.createElement(
+  'div',
+  {}, 
+  React.createElement('h2', {}, 'Lets get Started')),
+  React.createElement(Expenses, {items: expenses});
+
+
+  // return (
+  //   <div>
+  //     <h2>Let's get started!</h2>
+
+  //     <Expenses items = {expenses}></Expenses>
+    
+  //   </div>
+  // );
+
 }
 
 export default App;
--- react-budget-tool/src/index.js
+++ react-budget-tool/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 
+
 //createRoot means it tells react to make this the main place to render stuff through JS.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
